78a08f99ad281c0affe9522d36be21dc91b8e2d8
pawanwani <pawanwani1771@gmail.com>
2019-09-11 18:07:59 +0530

    updateaccount

--- app/src/main/java/com/maddy/adiii/mvpkrushibasket/Customer/Fragments/C_offers_fragment.java
+++ app/src/main/java/com/maddy/adiii/mvpkrushibasket/Customer/Fragments/C_offers_fragment.java
@@ -1,17 +1,22 @@
 package com.maddy.adiii.mvpkrushibasket.Customer.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.maddy.adiii.mvpkrushibasket.R;
+import com.maddy.adiii.mvpkrushibasket.login_reg;
 
 public class C_offers_fragment extends Fragment {
 	
 	private View rootView;
+	private Button GotoLogin;
 	
 	public C_offers_fragment(){
 	
@@ -23,8 +28,15 @@ public class C_offers_fragment extends Fragment {
 	        @Nullable Bundle savedInstanceState) {
 		
 		rootView = inflater.inflate(R.layout.fragment_c_offers, container, false);
-		
-		
+
+		GotoLogin=(Button)rootView.findViewById(R.id.btnGotologin);
+		GotoLogin.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i= new Intent(getContext(), login_reg.class);
+				startActivity(i);
+			}
+		});
 		return rootView;
 	}
 	
